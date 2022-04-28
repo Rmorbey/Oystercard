6966e94572d07ae7c50eef40b4fcb95504b74225
Russell <russellmorbey@hotmail.co.uk>
2022-04-28 14:37:06 +0100

    Added spec.

---
+++ spec/journey_spec.rb
@@ -0,0 +1,35 @@
+require 'oystercard'
+require 'station'
+
+describe Journey do
+
+  it 'can start a journey' do
+    expect(subject).to be_instance_of(Journey)
+  end
+
+  it 'can finish a journey' do
+
+  end
+
+  it 'can calculate fare for normal journey' do
+
+  end
+
+  it 'it charges penalty fare when no touch in' do 
+
+  end
+
+  it 'it charges penalty fare when no touch out' do
+
+  end
+
+  it 'it returns when a journey is complete' do
+
+  end
+
+  it 'returns when a journey is not complete' do
+
+  end
+
+
+end
